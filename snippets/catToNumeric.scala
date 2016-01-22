78e219262cbd1f80fa77c4031f800d1f6d874f9c
Ben <benrifkind>
2016-01-22 17:20:14 -0500

    decision tree on multiple categories

--- snippets/catToNumeric.scala
+++ snippets/catToNumeric.scala
@@ -1,20 +1,30 @@
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
-def age_new() = census.select("age", "Response")
-    .map(row => LabeledPoint(row.getString(1), Vectors.dense(row.getInt(0)))).toDF("response", ageVRBinary.columns(0))
-
+val data = sqlContext.read.format("libsvm").load("/home/ben/Documents/phoenix/sparkLearn/data/sample_libsvm_data.txt")
 
 def catToNumber(arg: String, df:DataFrame) : DataFrame = {
-      val indexer = new StringIndexer().setInputCol(arg).setOutputCol(arg+"_index")
+      val indexer = new StringIndexer().setInputCol(arg).setOutputCol(arg + "_index")
       indexer.fit(df).transform(df)
 }
 
 val cats = List("workclass", "education", "marital-status", "sex")
-
 val small_census = census.select("age", "workclass", "education", "marital-status", "sex", "Response")
-
-val small_census_cats = cats.foldLeft(small_census)((df, arg) => catToNumber(arg, df)).
-    select("age" :: cats.map(x=>x+"_index"))
+val small_census_cats = ("Response" :: cats).foldLeft(small_census)((df, arg) => catToNumber(arg, df))
 
 
-val a = Array("age", "workclass", "education")
+// import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+
+val ageDF = small_census_cats.map(row => LabeledPoint(row.getDouble(6), Vectors.dense(row.getInt(0)))).toDF()
+val indexer = new StringIndexer().setInputCol("label").setOutputCol("label_index")
+val ageDF2 = indexer.fit(ageDF).transform(ageDF)
+
+
+val dt = new DecisionTreeClassifier()
+  .setLabelCol("label_index")
+  .setFeaturesCol("features")
+
+val model_dt = dt.fit(ageDF2)

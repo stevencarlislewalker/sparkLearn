7cd35bd643bd9d66d4c77e78ac3502c2a04a4b30
stevencarlislewalker <swalker@angoss.com>
2016-01-25 14:27:43 -0500

    small edits and formatting

--- snippets/LogisticExample.scala
+++ snippets/LogisticExample.scala
@@ -1,11 +1,20 @@
-// Build a logistic regression. Assumes that we have a data frame in the correct form. Specifically of the form (label, features) with those names corresponding to those columns. The pipeline of scripts to run is readInCSV.scala, then catToNumerica.scala, and then this.
+// Build a logistic regression. Assumes that we have a data frame in
+// the correct form. Specifically of the form (label, features) with
+// those names corresponding to those columns. The pipeline of scripts to
+// run is readInCSV.scala, then catToNumerica.scala, and then this.
 
 import org.apache.spark.ml.classification.LogisticRegression
 
-// df is the name of the data frame we are building a model on. Use the censusDF frame constructed in catToNumeric
+// df is the name of the data frame we are building a model on. Use
+// the censusDF frame constructed in catToNumeric
 
 val df = censusDF
 
-// setting column names for regression. defaults are: (label, features) for (dependent, independent). These are also the defaults when we mapped RDD of LabeledPoints to dataframe.  
-val lr = new LogisticRegression().setLabelCol("label").setFeaturesCol("features")
+// setting column names for regression. defaults are: (label,
+// features) for (dependent, independent). These are also the defaults
+// when we mapped RDD of LabeledPoints to dataframe.
+
+val lr = new LogisticRegression()
+    .setLabelCol("Response_index")
+    .setFeaturesCol("features")
 val model = lr.fit(df)

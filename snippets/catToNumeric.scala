f8a0736a47f11c6c6a6b99801e13c087d283f432
Ben Rifkind <brifkind@angoss.com>
2016-01-25 10:17:20 -0500

    separating scala code into tasks

--- snippets/catToNumeric.scala
+++ snippets/catToNumeric.scala
@@ -1,12 +1,19 @@
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.feature.{StringIndexer, IndexToString}
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.sql.types._
-
+import org.apache.spark.sql.Row
+import sqlContext.implicits._
+import org.apache.spark.mllib.regression.LabeledPoint
 
 // This script is meant to get a data frame into shape so that it can be fed into an mllib model. That means that all categorical data (strings) are encoded as doubles - Eg., ("Yes"/ "No") ==> (0/1). And that it ends up as a data frame of LabeledPoints.
 
+// the starting data frame is named census. Assumes we already have this data frame in spark-shell.
 
+// names of columns we want to select for. Change these if the data frame is different or you want to select different columns.
+val cats = List("workclass", "education", "marital-status", "sex") //categorical
+val num = List("age") // numeric (continuous)
+val label = "Response" // dependent variable
 
 // helper functions
 
@@ -14,6 +21,7 @@ import org.apache.spark.sql.types._
 // return a data frame
 def indexCategorical(arg: String, df:DataFrame) : DataFrame = {
       val indexer = new StringIndexer()
+      indexer.setInputCol(arg).setOutputCol(arg + "_index")
       indexer.fit(df).transform(df)
 }
 
@@ -32,20 +40,11 @@ def toVector(row:Row, index:List[Int]):Vector = {
 
 // end helper functions
 
-// the starting data frame (we use census)
-val census = sqlContext.read.format("com.databricks.spark.csv")
-            .option("header", "true") // Use first line of all files as header
-            .option("inferSchema", "true") // Automatically infer data types
-            .load("/Users/Ben/Documents/phoenix/sparkLearn/data/Census.csv")
 
-// names of columns we want to select for
-val cats = List("workclass", "education", "marital-status", "sex") //categorical
-val num = List("age") // numeric (continuous)
-val label = "Response" // dependent variable
 
 
 // convert all categories (including label) to numeric
-val census_cats = (label :: cats).foldLeft(census)((df, arg) => catToNumber(arg, df))
+val census_cats = (label :: cats).foldLeft(census)((df, arg) => indexCategorical(arg, df))
 
 // find indices of variables
 val columns = census_cats.columns.toList.zipWithIndex //column names with index
@@ -64,6 +63,7 @@ val censusRDD = census_cats.map(row =>
         LabeledPoint(row.getDouble(label_index), toVector(row, index))
         )
 
+// the output data frame that we can build models on
 val censusDF = censusRDD.toDF
 
 
---
+++ snippets/readInCSV.scala
@@ -0,0 +1,14 @@
+// Read in a csv file as spark DataFrame. Specifically the census data set.
+
+// use spark-csv to read in census so start shell with
+// spark-shell --packages com.databricks:spark-csv_2.10:1.3.0
+
+import org.apache.spark.sql.SQLContext
+
+// CHANGE to file path of CSV file of your choice
+val filePath = "/home/ben/Documents/phoenix/sparkLearn/data/Census.csv"
+
+val census = sqlContext.read.format("com.databricks.spark.csv")  
+            .option("header", "true") // Use first line of all files as header
+            .option("inferSchema", "true") // Automatically infer data types 
+            .load(filePath) 

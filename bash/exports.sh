ca5f90c1b7ac991d7e720be70b8d638f40e28ec5
stevencarlislewalker <swalker@angoss.com>
2016-05-13 14:03:07 -0400

    .bashrc helpers

---
+++ bash/exports.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export SCALA_HOME=/usr/local/share/scala-2.11.7
+export SPARK_HOME=/usr/local/share/spark-1.6.0-bin-hadoop2.6
+
+export PATH="$PATH:$SCALA_HOME/bin:$SPARK_HOME/bin"

beb536cb488da7b628693a27a2cfee0d56bab9cd
stevencarlislewalker <swalker@angoss.com>
2016-12-29 13:47:16 -0500

    emacs init file from sparkLearn

--- elisp/init.el
+++ elisp/init.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
 (setq initial-frame-alist '((foreground-color . "grey90")
 			    (background-color . "grey20")
 			    (left . 50)  ))
@@ -9,11 +11,12 @@
 			    (height . 44)))
 (set-default 'cursor-type 'box)
 
+;(R-mode 1)
 
-;;(R-mode 1)
+;(setq ess-history-directory "~")
+;(ess-toggle-underscore nil)
 
-;;(setq ess-history-directory "~")
-;;(ess-toggle-underscore nil)
+
 
 (global-set-key (kbd "M-[") 'insert-pair)
 (global-set-key (kbd "M-\"") 'insert-pair)
@@ -22,6 +25,48 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-command-list
+   (quote
+    (("LaTeXnonint" "%`%l -interaction=nonstopmode %(mode)%' %t" TeX-run-command nil t)
+     ("TeX" "%(PDF)%(tex) %(extraopts) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (plain-tex-mode texinfo-mode ams-tex-mode)
+      :help "Run plain TeX")
+     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
+      (latex-mode doctex-mode)
+      :help "Run LaTeX")
+     ("Makeinfo" "makeinfo %(extraopts) %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with Info output")
+     ("Makeinfo HTML" "makeinfo %(extraopts) --html %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with HTML output")
+     ("AmSTeX" "%(PDF)amstex %(extraopts) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (ams-tex-mode)
+      :help "Run AMSTeX")
+     ("ConTeXt" "texexec --once --texutil %(extraopts) %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt once")
+     ("ConTeXt Full" "texexec %(extraopts) %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt until completion")
+     ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+     ("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber")
+     ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
+     ("Print" "%p" TeX-run-command t t :help "Print the file")
+     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
+     ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
+     ("Xindy" "texindy %s" TeX-run-command nil t :help "Run xindy to create index file")
+     ("Check" "lacheck %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for correctness")
+     ("ChkTeX" "chktex -v6 %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for common mistakes")
+     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+     ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
  '(inhibit-startup-screen t)
  '(send-mail-function nil))
 (custom-set-faces
@@ -48,7 +93,7 @@
 ;; (add-to-list 'ess-style-alist
 ;;              '(my-style
 ;;                (ess-indent-level . 4)
-;;                (ess-first-continued-statement-offset . 0) ;; should it be 2?
+;;                (ess-first-continued-statement-offset . 2) ;; should it be 2? yes
 ;;                (ess-continued-statement-offset . 0)
 ;;                (ess-brace-offset . -4)
 ;;                (ess-expression-offset . 4)
@@ -63,25 +108,25 @@
 ;; ----------------------------------------------------------------------
 
 ;; ----------------------------------------------------------------------
-
+;; mydir-
+;;
+;; light-weight
 ;; ----------------------------------------------------------------------
-;; Brilliant:
-;; https://github.com/emacs-ess/ESS/issues/96#issuecomment-71984227
-(add-to-list 'ess-style-alist
-             '(my-style
-               (ess-indent-level . 4)
-               (ess-first-continued-statement-offset . 2) ;; should it be 2? yes
-               (ess-continued-statement-offset . 0)
-               (ess-brace-offset . -4)
-               (ess-expression-offset . 4)
-               (ess-else-offset . 0)
-               (ess-close-brace-offset . 0)
-               (ess-brace-imaginary-offset . 0)
-               (ess-continued-brace-offset . 0)
-               (ess-arg-function-offset . 4)
-           (ess-arg-function-offset-new-line . '(4))
-               ))
-(setq ess-default-style 'my-style)
+
+;; (defun mydir-ppg ()
+;;   (interactive)
+;;   (find-file (concat "//ca3answcmgr02/ClientData/PPG/Work/Services/")))
+
+;; (defun mydir-content-manager ()
+;;   (interactive)
+;;   (find-file (concat "//ca3answcmgr02/ClientData/")))
+
+;; (defun mydir-notes ()
+;;   (interactive)
+;;   (find-file (concat "~/Documents/notes/")))
+
+
+
 
 ;; (setq paragraph-start
 ;;       '("\f\\|[ \t]*$\\|[ \t]*[*+-] \\|[ \t]*[0-9]+\\.\\|[ \t]*: "))
@@ -102,8 +147,10 @@
 (put 'upcase-region 'disabled nil)
 
 
+
 ;; use linux line endings
 (setq default-buffer-file-coding-system 'utf-8-unix)
+
 
 
 (defun renumber (&optional num)
@@ -153,44 +200,6 @@
   (backward-char 75))
 
 
-;; (require' package)
-;; (add-to-list 'package-archives
-;; 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-;; (setq exec-path (append exec-path '("/usr/local/share/scala-2.11.7")))
-
-;;(require 'ensime)
-;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
-(add-to-list 'load-path "/home/steve/scala-tool-support/tool-support/emacs")
-(add-to-list 'load-path "/usr/local/share/scala-2.11.7")
-(require 'scala-mode-auto)
-
-(defun essSparkR ()
-  (interactive)
-  (R)
-  (insert "Sys.setenv\(SPARK_HOME = \"/usr/local/share/spark-1.6.0-bin-hadoop2.6\"); ")
-  (insert "library(SparkR, lib = file.path(Sys.getenv(\"SPARK_HOME\"), \"R\", \"lib\")); ")
-  (insert "sc <- sparkR.init(sparkPackages=\"com.databricks:spark-csv_2.11:1.0.3\"); ")
-  (insert "sqlContext <- sparkRSQL.init(sc)")
-  (inferior-ess-send-input))
-
-
-(defun bigcomment ()
-  (interactive)
-  (insert "#### ----------------------------------------------------------------------\n")
-  (insert "#### \n")
-  (insert "#### ----------------------------------------------------------------------\n")
-  (backward-char 77))
-
-(defun ligcomment ()
-  (interactive)
-  (insert "## ----------------------------------------------------------------------\n")
-  (insert "## \n")
-  (insert "## ----------------------------------------------------------------------\n")
-  (backward-char 75))
-
-
 
 (defun rpipe ()
   (interactive)
@@ -198,5 +207,61 @@
   (insert "%>%")
   (just-one-space 1))
 
-(define-key ess-mode-map (kbd "C-%") 'rpipe)
-(define-key inferior-ess-mode-map (kbd "C-%") 'rpipe)
+;; (define-key ess-mode-map (kbd "C-%") 'rpipe)
+;; (define-key inferior-ess-mode-map (kbd "C-%") 'rpipe)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; oh ya!
+;; http://stackoverflow.com/questions/6286579/emacs-shell-mode-how-to-send-region-to-shell#7053298
+(defun sh-send-line-or-region (&optional step)
+  (interactive ())
+  (let ((proc (get-process "shell"))
+        pbuf min max command)
+    (unless proc
+      (let ((currbuff (current-buffer)))
+        (shell)
+        (switch-to-buffer currbuff)
+        (setq proc (get-process "shell"))
+        ))
+    (setq pbuff (process-buffer proc))
+    (if (use-region-p)
+        (setq min (region-beginning)
+              max (region-end))
+      (setq min (point-at-bol)
+            max (point-at-eol)))
+    (setq command (concat (buffer-substring min max) "\n"))
+    (with-current-buffer pbuff
+      (goto-char (process-mark proc))
+      (insert command)
+      (move-marker (process-mark proc) (point))
+      ) ;;pop-to-buffer does not work with save-current-buffer -- bug?
+    (process-send-string  proc command)
+    (display-buffer (process-buffer proc) t)
+    (when step
+      (goto-char max)
+      (next-line))
+    ))
+
+(defun sh-send-line-or-region-and-step ()
+  (interactive)
+  (sh-send-line-or-region t))
+(defun sh-switch-to-process-buffer ()
+  (interactive)
+  (pop-to-buffer (process-buffer (get-process "shell")) t))
+
+;;(define-key sh-mode-map [(control ?j)] 'sh-send-line-or-region-and-step)
+;;(define-key sh-mode-map [(control ?c) (control ?z)] 'sh-switch-to-process-buffer)
+
+(setq python-shell-interpreter "pyspark")
+
+(add-hook 'python-mode-hook
+  (lambda ()
+    (setq indent-tabs-mode t)
+    (setq python-indent 4)
+    (setq tab-width 4))
+    (tabify (point-min) (point-max)))
+
+(whitespace-mode 1)
+(setq whitespace-style '(trailing tabs newline tab-mark newline-mark))
+
